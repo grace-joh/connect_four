333a4bf0457bc6f32b097954170cafa1a0eadb66
margdaly <margaret.daly@gmail.com>
2023-02-18 16:22:49 -0700

    feat: Add #print for Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,28 @@
 class Board
   attr_reader :layout 
-  
+
   def initialize
     @layout = {
-      A: ['*','*','*','*','*','*'],
-      B: ['*','*','*','*','*','*'],
-      C: ['*','*','*','*','*','*'],
-      D: ['*','*','*','*','*','*'],
-      E: ['*','*','*','*','*','*'],
-      F: ['*','*','*','*','*','*'],
-      G: ['*','*','*','*','*','*']
+      A: ["*","*","*","*","*","*"],
+      B: ["*","*","*","*","*","*"],
+      C: ["*","*","*","*","*","*"],
+      D: ["*","*","*","*","*","*"],
+      E: ["*","*","*","*","*","*"],
+      F: ["*","*","*","*","*","*"],
+      G: ["*","*","*","*","*","*"]
     }
+  end
+
+  def print
+    puts @layout.keys.join(" ")
+    index = 6
+    while index >= 0
+      print_array = []
+      @layout.values.each do |column_array|    
+        print_array << column_array[index]
+      end
+      puts print_array.join(" ")    
+      index -= 1
+    end
   end
 end
