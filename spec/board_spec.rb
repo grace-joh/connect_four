043331c8a12b04e459712f6304ccd51765404adb
margdaly <margaret.daly@gmail.com>
2023-02-19 12:59:02 -0700

    test: Test for column free and full in board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Board do
       expect { board.print_layout }.to output("A B C D E F G\n* * * * * * *\n* * * * * * *\n* * * * * * *\n* * * * * * *\n* * * * * * *\n* * * * * * *\n").to_stdout
     end
   end
+
+  describe '#column_free?' do
+    it 'checks if column has available spaces' do
+      board = Board.new
+      computer = Computer.new
+      input = computer.input
+
+      expect(board.column_free?(input)).to eq(true)
+      expect(board.column_free?(:A)).to eq(true)
+      expect(board.column_free?(:B)).to eq(true)
+      expect(board.column_free?(:C)).to eq(true)
+      expect(board.column_free?(:D)).to eq(true)
+      expect(board.column_free?(:E)).to eq(true)
+      expect(board.column_free?(:F)).to eq(true)
+      expect(board.column_free?(:G)).to eq(true)
+    end
+
+    it 'returns false if column is full' do
+      board = Board.new
+      board.layout[:A] = ['X','X','X','X','X','X']
+      board.layout[:B] = ['O','X','O','X','O','X']
+      board.layout[:C] = ['O','X','O','X','O','*']
+
+      expect(board.column_free?(:A)).to eq(false)
+      expect(board.column_free?(:B)).to eq(false)
+      expect(board.column_free?(:C)).to eq(true)
+    end
+  end
 end
