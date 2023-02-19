bd709bcb46b9b8dd876b0066a1d3cde55b44cfe0
Grace Joh <graceehjoh@gmail.com>
2023-02-19 10:31:51 -0600

    test: add second test for Computer #initialize

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe Computer do
       expect(computer.symbol).to eq('O')
       expect(computer.input).to be_nil
     end
+
+    it 'can initialize another object' do
+      computer2 = Computer.new
+
+      expect(computer2).to be_a(Computer)
+      expect(computer2.symbol).to eq('O')
+      expect(computer2.input).to be_nil
+    end
   end
 end
