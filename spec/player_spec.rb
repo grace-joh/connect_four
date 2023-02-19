3b46fa96e00f20b8c65488c3956f0c3712dcbd82
margdaly <margaret.daly@gmail.com>
2023-02-19 09:34:35 -0700

    fix: Fix initialize test and add edge case for player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,8 +6,16 @@ RSpec.describe Player do
       player = Player.new
 
       expect(player).to be_a(Player)
-      expect(player.symbol).to be_a(String)
-      expect(player.input).to eq(nil)
+      expect(player.symbol).to be_a('X')
+      expect(player.input).to be_nil
+    end
+
+    it 'can initialize another object' do
+      player2 = Player.new 
+
+      expect(player2).to be_a(Player)
+      expect(player2.symbol).to be_a('X')
+      expect(player2.input).to be_nil
     end
   end
 end
