70600dd58bbbcf4c22cc629c7e27120bf20deee2
Grace Joh <graceehjoh@gmail.com>
2023-02-21 14:24:15 -0600

    feat: create Game class with #initialize

---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Game
+  attr_reader :player1, :player2, :turn, :winner
+
+  def initialize
+    @player1 = Player.new
+    @player2 = Computer.new
+    @turn = Turn.new
+    @winner = @turn.winner
+  end
+end
