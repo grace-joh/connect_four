9660f4546313daeb96fdaf8c3d430a2594f53fa8
Grace Joh <graceehjoh@gmail.com>
2023-02-21 14:27:53 -0600

    test: add test for Game #players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe Game do
       expect(@game.winner).to be_nil
     end
   end
+
+  describe '#players' do
+    it 'returns an array of both players' do
+      expect(@game.players).to eq([@game.player1, @game.player2])
+      expect(@game.players.first).to be_a(Player)
+      expect(@game.players.last).to be_a(Computer)
+    end
+  end
 end
