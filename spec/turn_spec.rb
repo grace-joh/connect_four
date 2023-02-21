a5837b4987831afb7ef27f0b63f28ef5831044a4
margdaly <margaret.daly@gmail.com>
2023-02-21 12:57:23 -0700

    Update tests to turn.board

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Turn do
       input = 'A'
       turn.update_board(player, input)
 
-      expect(@board.layout[:A]).to eq(['X', '*', '*', '*', '*', '*'])
+      expect(turn.board.layout[:A]).to eq(['X', '*', '*', '*', '*', '*'])
     end
 
     it 'adds a second piece to the column the player chooses' do
@@ -38,7 +38,7 @@ RSpec.describe Turn do
       input = 'A'
       turn.update_board(computer, input)
 
-      expect(@board.layout[:A]).to eq(['X', 'O', '*', '*', '*', '*'])
+      expect(turn.board.layout[:A]).to eq(['X', 'O', '*', '*', '*', '*'])
     end
 
     it 'can add a piece to another column the player chooses' do
@@ -52,8 +52,8 @@ RSpec.describe Turn do
       input = 'B'
       turn.update_board(user, input)
 
-      expect(@board.layout[:A]).to eq(['X', 'O', '*', '*', '*', '*'])
-      expect(@board.layout[:B]).to eq(['X', '*', '*', '*', '*', '*'])
+      expect(turn.board.layout[:A]).to eq(['X', 'O', '*', '*', '*', '*'])
+      expect(turn.board.layout[:B]).to eq(['X', '*', '*', '*', '*', '*'])
     end
   end
 
@@ -71,9 +71,9 @@ RSpec.describe Turn do
     it 'player is the winner if players move wins' do
       user = Player.new
       turn = Turn.new
-      @board.layout[:B] = ['X','*','*','*','*','*']
-      @board.layout[:C] = ['X','*','*','*','*','*']
-      @board.layout[:D] = ['X','*','*','*','*','*']
+      turn.board.layout[:B] = ['X','*','*','*','*','*']
+      turn.board.layout[:C] = ['X','*','*','*','*','*']
+      turn.board.layout[:D] = ['X','*','*','*','*','*']
       turn.move(user) # when user input is A or E
 
       expect(turn.winner).to eq(user)
@@ -82,13 +82,13 @@ RSpec.describe Turn do
     it 'computer is the winner if computers move wins' do
       computer = Computer.new
       turn = Turn.new
-      @board.layout[:A] = ['O','O','O','*','*','*']
-      @board.layout[:B] = ['O','O','O','*','*','*']
-      @board.layout[:C] = ['O','O','O','*','*','*']
-      @board.layout[:D] = ['*','*','*','*','*','*']
-      @board.layout[:E] = ['O','O','O','*','*','*']
-      @board.layout[:F] = ['O','O','O','*','*','*']
-      @board.layout[:G] = ['O','O','O','*','*','*']
+      turn.board.layout[:A] = ['O','O','O','*','*','*']
+      turn.board.layout[:B] = ['O','O','O','*','*','*']
+      turn.board.layout[:C] = ['O','O','O','*','*','*']
+      turn.board.layout[:D] = ['*','*','*','*','*','*']
+      turn.board.layout[:E] = ['O','O','O','*','*','*']
+      turn.board.layout[:F] = ['O','O','O','*','*','*']
+      turn.board.layout[:G] = ['O','O','O','*','*','*']
       turn.move(computer)
 
       expect(turn.winner).to eq(computer)
@@ -105,7 +105,7 @@ RSpec.describe Turn do
 
     it 'returns false if the input column is full' do
       turn = Turn.new
-      @board.layout[:G] = ['O','O','O','O','O','O']
+      turn.board.layout[:G] = ['O','O','O','O','O','O']
       input = 'G'
 
       expect(turn.valid?(input)).to eq(false)
