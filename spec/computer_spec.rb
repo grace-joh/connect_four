c37c5447282242ee4dd56cee19a04a0ab95d0280
margdaly <margaret.daly@gmail.com>
2023-02-22 15:50:13 -0700

    Add agrument to computer and check for #inhert

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
+# require './player'
 
 RSpec.describe Computer do
   describe '#initialize' do
     it 'exists' do
-      computer = Computer.new
+      computer = Computer.new('O')
 
       expect(computer).to be_a(Computer)
       expect(computer.symbol).to eq('O')
     end
 
     it 'can initialize another object' do
-      computer2 = Computer.new
+      computer2 = Computer.new('O')
 
       expect(computer2).to be_a(Computer)
       expect(computer2.symbol).to eq('O')
@@ -19,11 +20,19 @@ RSpec.describe Computer do
 
   describe '#input' do
     it 'returns a letter from A to G randomly' do
-      computer = Computer.new
+      computer = Computer.new('O')
 
       expect(computer.input).to be_a(String)
       expect(('A'..'G').to_a).to include(computer.input)
       expect(('H'..'Z').to_a).not_to include(computer.input)
+    end
+  end
+
+  describe '#inherit' do
+    it 'inherits behavior from Player' do
+      computer = Computer.new('O')
+
+      expect(Computer.superclass).to eq(Player)
     end
   end
 end
