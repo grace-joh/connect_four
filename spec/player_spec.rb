7006f2b1699722a25e5433b215915c8d1287b1ee
Grace Joh <graceehjoh@gmail.com>
2023-02-22 15:42:21 -0600

    refactor: #initialize tests to include player type

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,17 +3,19 @@ require 'spec_helper'
 RSpec.describe Player do
   describe '#initialize' do
     it 'exists' do
-      player = Player.new
+      player = Player.new('human', 'X')
 
       expect(player).to be_a(Player)
-      expect(player.symbol).to be_a('X')
+      expect(player.type).to eq('human')
+      expect(player.symbol).to eq('X')
     end
 
     it 'can initialize another object' do
-      player2 = Player.new 
+      player2 = Player.new('dog', 'O')
 
       expect(player2).to be_a(Player)
-      expect(player2.symbol).to be_a('X')
+      expect(player2.type).to eq('dog')
+      expect(player2.symbol).to eq('O')
     end
   end
 end
